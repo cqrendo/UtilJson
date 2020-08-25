1b4fb784364f444ae7036ac6654fc017709fb70f
Yolanda Quinteiro <yolaqr@gmail.com>
2020-08-25 11:57:31 +0200

    Login con name y description en el appconst

--- JavaToCopy/coop/intergal/ui/views/login/LoginView.java
+++ JavaToCopy/coop/intergal/ui/views/login/LoginView.java
@@ -1,11 +1,16 @@
 package coop.intergal.ui.views.login;
 
+import java.util.Locale;
+
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.Route;
+
+import coop.intergal.AppConst;
 
 @Route(value = "login")
 public class LoginView extends FlexLayout implements AfterNavigationObserver {
@@ -13,10 +18,14 @@ public class LoginView extends FlexLayout implements AfterNavigationObserver {
     private final LoginOverlay login;
 
     public LoginView(){
+		UI.getCurrent().setLocale(new Locale("es", "ES"));
+
         login = new LoginOverlay();
 
         login.setForgotPasswordButtonVisible(true);
         login.setAction("login");
+        login.setTitle(AppConst.LOGIN_NAME);
+        login.setDescription(AppConst.LOGIN_DESCRIPTION);
 
         add(login);
     }
